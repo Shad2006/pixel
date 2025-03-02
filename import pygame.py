0bdd7abea3a7701cd59f99910ee7c29615d7f436
Shad <66088327+Shad2006@users.noreply.github.com>
2025-03-02 10:00:48 +0300

    3. added blocks

--- import pygame.py
+++ import pygame.py
@@ -12,12 +12,21 @@ ball_speed = 6
 ball_d = ball_r * 2
 ball = pygame.Rect(randrange(ball_d, width - ball_d), height / 2, ball_d, ball_d)
 dx = 1
-dy = 1
+dy = -1
 paddle = pygame.Rect(width/2 - paddle_w/2, height - paddle_h - 10, paddle_w, paddle_h)
 p_speed = 15
 sc = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-
+blocks = [
+    pygame.Rect(10+120*i, 10+70*j, 100, 50)
+    for i in range(10)
+    for j in range(5)
+]
+colors = [
+    (randrange(0, 256), randrange(0, 256), randrange(0, 256))
+    for i in range(10)
+    for j in range(5)
+]
 while True:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
@@ -35,11 +44,20 @@ while True:
 
     ball.x += ball_speed * dx
     ball.y += ball_speed * dy
-    
+    if ball.centerx<ball_r or ball.centerx>width-ball_r:
+        dx=-dx
+        #при else мяч движется только влево-вправо
+    elif ball.centery < ball_r or ball.centery > height - ball_r:
+        dy = -dy
+    elif ball.colliderect(paddle) and dy > 0:
+        dy = -dy
 
     pygame.draw.rect(sc, pygame.Color("Orange"), paddle)
     pygame.draw.circle(sc, pygame.Color("white"), ball.center, ball_r)
-    
+    [
+        pygame.draw.rect(sc, colors[color], block)
+        for color, block in enumerate(blocks)
+    ]
 
     pygame.display.flip()
     clock.tick(fps)
